d96fba5e327cf00067f8c5ad442ec6183a6a1cad
unknown <evandriridho555@gmail.com>
2024-07-09 16:47:07 +0700

    Set Up Tic Tac Toe React Js

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function Square() {
+  return <button>X</button>
+}
+
+function App() {
+  return (
+    <>
+      <Square/>
+    </>
+  )
+}
+
+export default App
